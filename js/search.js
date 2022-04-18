9ba25ab2f6b4807f3478b02f2068fc04453a1571
Mirko-r <04mirko@libero.it>
2022-04-18 10:19:11 +0200

    Try to add search bar

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
 var sjs = SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
-  json: '/search.json'
+  json: '/search.json',
+  searchResultTemplate: '<li><a href="{{ site.url }}{url}">{title}</a></li>'
 })
